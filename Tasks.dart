4d1136d3746f843bc1d4a752565c5685d98e472e
1hp-228322 <keshasuper396@gmail.com>
2025-12-22 14:57:46 +0300

    Create Tasks.dart

---
+++ Tasks.dart
@@ -0,0 +1,55 @@
+//Задание 1
+int sum(int a, int b){
+  return a+ b;
+}
+void main() {
+print('Результат работы функции sum = ${sum(10,10)} ');
+}
+//Задание 2
+int multiply(int a, int b){
+  return a * b;
+}
+void main() {
+print('Результат работы функции multiply = ${multiply(10,10)} ');
+}
+//Задание 3
+double divide(double a, double b){
+  return a / b;
+}
+void main() {
+print('Результат работы функции divide = ${divide(10,10)} ');
+}
+//Задание 4
+double remainder(double a, double b){
+  return a % b;
+}
+void main() {
+print('Результат работы функции divide = ${remainder(13,3)} ');
+}
+//Задание 5
+import 'dart:math';
+num power (num base, num exponent){
+  return pow(base, exponent);
+}
+void main() {
+print('Результат работы функции divide = ${power(3,3)} ');
+}
+//Задание 6
+int factorial(int n){
+  int result = 1;
+  for(int i = 1; i <=n; i++){
+    result *=1;
+  }
+  return result;
+}
+
+void main(){
+  print('Результат работы функции factorial = ${factorial(5)}');
+}
+//Задание 7
+
+//Задание 8
+
+//Задание 9
+
+//Задание 10
